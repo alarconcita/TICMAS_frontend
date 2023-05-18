b2cf250f4b0f3a3f9f43047c5e64052a14fe7780
Mariana Alarcón <124741193+alarconcita@users.noreply.github.com>
2023-05-17 22:16:57 -0300

    Add files via upload

---
+++ JS/cv.js
@@ -0,0 +1,10 @@
+document.getElementById('telefono').onclick=function(){
+    console.log("Capturamos el click")
+    document.getElementById('telefono').innerHTML = "+54 03493 15459749"
+}
+
+
+document.getElementById('email').onclick=function(){
+    console.log("Capturamos el click")
+    document.getElementById('email').innerHTML = "alarconcita@gmail.com"
+}
